2606dfaa6ed29b46f97bf985fdc50c2a99444d3b
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-04-11 19:53:31 +0300

    Find todos in files

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use std::{env::current_dir, path::Path};
-use walkdir::WalkDir;
+use std::{env::current_dir, path::Path, collections::HashSet};
+use walkdir::{WalkDir, DirEntry};
 
 #[tokio::main]
 async fn main() {
     // get files from current directory
     let cur_dir = current_dir().unwrap();
-    if let Some(asdf) = ignore_files(&cur_dir) {
+    if let Some(to_ignore) = ignore_files(&cur_dir) {
         WalkDir::new(".")
         .into_iter()
-        .filter_entry(|e| filter_files(&asdf, e.path()) || !e.path().display().to_string().starts_with("."))
+        .filter_entry(|e| filter_files(&to_ignore, e.path()))
         .filter_map(|v| v.ok())
-        .for_each(|x| println!("{}", x.path().display()));
+        .for_each(|x| {
+            let todos = find_todos(x);
+            if todos.len() > 0 {
+                for todo in todos {
+                    println!("\t{}", todo);
+                }
+            } 
+        });
     }
 }
 
@@ -19,11 +26,30 @@ fn filter_files(ignore: &Vec<String>, path: &Path) -> bool {
     !ignore.iter().any(|x| path_str.contains(x))
 }
 
+fn find_todos(dir: DirEntry) -> HashSet<String> {
+    let path = dir.path();
+    let mut unique_todos: HashSet<String> = HashSet::new();
+    if path.is_file() {
+        let contents = std::fs::read_to_string(path).unwrap();
+        let todos: Vec<_> = contents.match_indices("TODO").collect();
+        if todos.len() > 0 {
+            for (_, todo) in todos.iter().enumerate() {
+                let mut todo_str = String::new();
+                for c in contents.chars().skip(todo.0).take_while(|c| c != &'\n') {
+                    todo_str.push(c);
+                }
+                unique_todos.insert(todo_str);
+            }
+        }
+    }
+    return unique_todos;
+}
 
 fn ignore_files(cur_dir: &std::path::PathBuf) -> Option<Vec<String>> {
     for entry in cur_dir.read_dir().unwrap() {
         if let Ok(entry) = entry {
             // if file is gitignore, read its contents
+            // TODO: add support for other ignore files
             if entry.path().to_str().unwrap().ends_with(".gitignore") {
                 let contents = std::fs::read_to_string(entry.path()).unwrap();
                 // append to ignore folders from contents
