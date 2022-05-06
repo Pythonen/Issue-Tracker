7a634039d058aeb89a2a0042fe5d5bae8b5dc8e6
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-05-06 19:37:27 +0300

    Args: Write some simple tests for `args` module

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use super::actions;
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
-struct CliArgs {
+pub struct CliArgs {
     // Find all todos in the project
     #[clap(subcommand)]
     cmds: SubCommand
@@ -15,15 +15,56 @@ enum SubCommand {
     Init
 }
 
-pub fn parse_arguments() {
-    let args = CliArgs::parse();
+pub fn parse_arguments(args: CliArgs) -> Result<(), String> {
     if args.cmds == SubCommand::Init {
+        // TODO: Check that the operation succeeds
         actions::init_new_project();
+        return Ok(());
     }
     else if args.cmds == SubCommand::Print {
+        // TODO: Check that the operation succeeds
         actions::print_todos();
+        return Ok(());
     }
     else if args.cmds == SubCommand::Report {
+        // TODO: Check that the operation succeeds !! IMPORTANT !!
         actions::report_todos();
+        return Ok(());
+    }
+    return Err("Unknown command".to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_arguments;
+
+    use super::{CliArgs, SubCommand};
+    #[test]
+    fn test_parse_arguments_with_init() {
+        let args = CliArgs{
+            cmds: SubCommand::Init
+        };
+        let parsed = parse_arguments(args);
+        assert!(parsed.is_ok());
+    }
+
+    #[test]
+    #[should_panic]
+    // TODO: This should panic only for now as this command is not yet implemented
+    fn test_parse_arguments_with_report() {
+        let args = CliArgs{
+            cmds: SubCommand::Report
+        };
+        let parsed = parse_arguments(args);
+        assert!(parsed.is_err());
+    }
+
+    #[test]
+    fn test_parse_arguments_with_print() {
+        let args = CliArgs{
+            cmds: SubCommand::Print
+        };
+        let parsed = parse_arguments(args);
+        assert!(parsed.is_ok());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod args;
 mod util;
 mod actions;
-use args::parse_arguments;
+use args::{parse_arguments, CliArgs};
+use clap::StructOpt;
 
 #[tokio::main]
 async fn main() {
-    parse_arguments();
+    let args = CliArgs::parse();
+    let _ = parse_arguments(args);
     // get files from current directory
 }
 
