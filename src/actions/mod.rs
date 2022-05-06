53ba304a5540696cd9be6cd898e4e2cafd0e6a69
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2022-05-06 19:11:56 +0300

    Actions: Make wrapper action that takes a closure
    Made an separate action "wrapper" sort of that performs the traversal
    of todos. You can then pass the individual todo for the closure the
    wrapper accepts.

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -25,3 +25,56 @@ pub fn init_new_project() -> Config {
                 }
             }
 }
+
+fn find_todos_wrapper(fun: fn(String) -> ()) {
+    let cur_dir = current_dir().unwrap();
+    if let Some(to_ignore) = ignore_files(&cur_dir) {
+        WalkDir::new(".")
+        .into_iter()
+        .filter_entry(|e| filter_files(&to_ignore, e.path()))
+        .filter_map(|v| v.ok())
+        .for_each(|x| {
+            let todos = find_todos(x);
+            if todos.len() > 0 {
+                for todo in todos {
+                    fun(todo);
+                }
+            }
+        })
+    }
+}
+
+pub fn report_todos() {
+    todo!()
+}
+
+pub fn print_todos() {
+    // TODO: Make formatting of this more userfriendly and pretty
+    find_todos_wrapper(|todo| {
+        println!("{:_^40}", "TODOS");
+        println!("{}", todo);
+        println!("\n----------------------------------------\n");
+    });
+}
+
+fn find_todos(dir: DirEntry) -> HashSet<String> {
+    let path = dir.path();
+    let mut unique_todos: HashSet<String> = HashSet::new();
+    if path.is_file() && path.extension().is_some() {
+        if let Ok(contents) = std::fs::read_to_string(path) {
+            let todos: Vec<_> = contents.match_indices("TODO").collect();
+            if todos.len() > 0 {
+                for (_, todo) in todos.iter().enumerate() {
+                    let mut todo_str = String::new();
+                    let file_for_todo = format!("{}: ", path.display());
+                    todo_str.push_str(&file_for_todo);
+                    for c in contents.chars().skip(todo.0).take_while(|c| c != &'\n') {
+                        todo_str.push(c);
+                    }
+                    unique_todos.insert(todo_str);
+                }
+            }
+        }
+    }
+    return unique_todos;
+}
